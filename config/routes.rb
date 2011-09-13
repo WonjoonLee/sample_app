9a3ac803788dc88b782abf418259022416b16f64
Wonjoon Lee <wlee@fmginc.com>
2011-09-13 11:39:43 -0400

    Added layout links (with routes)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
+  root :to => 'pages#home'
   
-  get "pages/about"
+  match '/contact', :to => 'pages#contact'
+  
+  match '/about', :to => 'pages#about'
+  
+  match '/help', :to => 'pages#help'
+  
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
