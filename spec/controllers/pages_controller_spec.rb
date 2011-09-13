d539b7997abe265a59f53b10cdf5bd1570908573
Wonjoon Lee <wlee@fmginc.com>
2011-09-13 09:28:41 -0400

    Added a site layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -14,6 +14,11 @@ describe PagesController do
       response.should have_selector("title",
                         :content => "Ruby on Rails Tutorial Sample App | Home")
     end
+    
+    it "should have a non-blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
   end
 
   describe "GET 'contact'" do
