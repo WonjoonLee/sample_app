fbcd825a6501ce2e1a1522cdf4baae35c19767d4
Wonjoon Lee <wlee@fmginc.com>
2011-09-14 16:33:26 -0400

    User model validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110913171558) do
+ActiveRecord::Schema.define(:version => 20110914202944) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,5 +19,7 @@ ActiveRecord::Schema.define(:version => 20110913171558) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
