c55a577ac4c5e7804c7a4e2066b3109055572c30
Wonjoon Lee <wlee@fmginc.com>
2011-09-20 16:53:04 -0400

    Added a Sessions controller and a signin view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
   
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   root :to => 'pages#home'
   
@@ -12,7 +13,10 @@ SampleApp::Application.routes.draw do
   
   match '/signup', :to => 'users#new'
   
-
+  match '/signin', :to => 'sessions#new'
+  
+  match '/signout', :to => 'sessions#destroy'
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -27,6 +27,10 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Sign Up")
   end
   
+  it "should have a Signin page at '/signin'" do
+    get '/signin'
+    response.should have_selector('title', :content => "Sign in")
+  end
   
   it "should have the right links on the layout" do
     visit root_path
