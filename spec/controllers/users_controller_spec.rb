c99976359a534e224e25c2e1490f78681d087ece
Wonjoon Lee <wlee@fmginc.com>
2011-09-26 15:46:27 -0400

    Friendly forwarding and correct user requirement

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -187,15 +187,37 @@ describe UsersController do
       @user = Factory(:user)
     end
     
-    it "should deny access to 'edit'" do
-      get :edit, :id => @user
-      response.should redirect_to(signin_path)
-      flash[:notice].should =~ /sign in/i
+    describe "for non-signed in users" do
+      
+      it "should deny access to 'edit'" do
+        get :edit, :id => @user
+        response.should redirect_to(signin_path)
+        flash[:notice].should =~ /sign in/i
+      end
+
+      it "should dent access to 'update'" do
+        put :update, :id => @user, :user => {}
+        response.should redirect_to(signin_path)
+      end
+      
     end
+    describe "for signed in users" do
     
-    it "should dent access to 'update'" do
-      put :update, :id => @user, :user => {}
-      response.should redirect_to(signin_path)
+      before(:each) do
+        wrong_user = Factory(:user, :email => "good@name.net")
+        test_sign_in(wrong_user)
+      end
+      
+      it "should require matching users for 'edit'" do
+        get :edit, :id => @user
+        response.should redirect_to(root_path)
+      end
+      
+      it "should require matching users for 'update'" do
+        put :update, :id => @user, :user => {}
+        response.should redirect_to(root_path)
+      end
+      
     end
     
   end
